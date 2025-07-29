2512262a679333e5ae93bd8bda9cb10ca7c8dc2d
Zishen Chen <zche4727@uni.sydney.edu.au>
2025-07-29 18:14:37 +1000

    system portotype

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+
+'''
+Customise the admin page by pre-setting django
+'''
+class MenuItemAdmin(admin.ModelAdmin):
+    '''Adding the functions in admin page'''
+    list_display = ("meal", "status") # show the fields 
+    list_filter = ("status",)
+    search_fields = ("meals", "description")
+
+'''
+Linked both class into admin page
+'''
+admin.site.register(Item, MenuItemAdmin)
+
+'''
+The adding item format is all configured in the models.py in app directory
+'''
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Item, MEAL_TYPE # own created dish class
 
 # Create your views here.
+'''
+The following page when user click dish title
+When user trigger the url, both class html need to activated
+'''
+class MenuList(generic.ListView):
+    queryset = Item.objects.order_by("-date_created") # The Item class from models.py
+    template_name = "index.html" # it's menu front page
+
+    '''
+    Send backend content to html 
+    '''
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE # dictionary data structure
+        return context
+
+class MenuItemDetail(generic.DeleteView):
+    model = Item
+    template_name = "menu_item_detail.html" # it's individual item page once user click
+
+'''
+The templates directory is updated inside the app directory
+We can make common-html-template in django mysite as well
+'''
